620c80992d1c6b8e133d3858fc92aac8a3eced5f
notPlancha <andre.plancha@hotmail.com>
2021-05-20 16:58:00 +0100

    Added volume-profiles/

---
+++ volume-profiles/README.md
@@ -0,0 +1,24 @@
+# Volume Profiler
+
+Spicetify extension to set your current Spotify volume into a saved volume
+
+## Preview
+
+![Buttons screenshot](Buttons.png)
+
+A right click on the new buttons will let you save your current volume to that profile
+
+![Popup screenshot](popup.png)
+
+
+## Notes
+
+* It's possible that an update to spotify or spicetify will break your saved volumes, but I've yet to experience this. If this happens, try raising an issue on the [Volume Profile issues's github page](https://github.com/notPlancha/volume-profiles-spicetify/issues)
+  * Same thing for any other issue/bug
+* This extension uses Uzair Farooq's [arrive.js](https://github.com/uzairfarooq/arrive)
+
+## More
+
+* Check out me ([@notPlancha](https://github.com/notPlancha)) for possibly future extensions, and check the where the repository is updated and stuff https://github.com/notPlancha/volume-profiles-spicetify
+
+* I might do a similar extension but with windows volume evaluation, depending on reception and interest. If you have any suggestions, drop it on the [issues's page](https://github.com/notPlancha/volume-profiles-spicetify/issues) or a direct pull request
---
+++ volume-profiles/volumeProfiles.js
@@ -0,0 +1,78 @@
+// @ts-check
+
+// NAME: Volume Profiles
+// AUTHOR: notPlancha
+// DESCRIPTION: Create volume profiles to have better control of your Spotify Volume, using Spicetify
+
+/// <reference path="../globals.d.ts" />
+
+//Thanks for Uzair Farooq for this library [https://github.com/uzairfarooq/arrive]
+var Arrive=function(e,t,n){"use strict";function r(e,t,n){l.addMethod(t,n,e.unbindEvent),l.addMethod(t,n,e.unbindEventWithSelectorOrCallback),l.addMethod(t,n,e.unbindEventWithSelectorAndCallback)}function i(e){e.arrive=f.bindEvent,r(f,e,"unbindArrive"),e.leave=d.bindEvent,r(d,e,"unbindLeave")}if(e.MutationObserver&&"undefined"!=typeof HTMLElement){var o=0,l=function(){var t=HTMLElement.prototype.matches||HTMLElement.prototype.webkitMatchesSelector||HTMLElement.prototype.mozMatchesSelector||HTMLElement.prototype.msMatchesSelector;return{matchesSelector:function(e,n){return e instanceof HTMLElement&&t.call(e,n)},addMethod:function(e,t,r){var i=e[t];e[t]=function(){return r.length==arguments.length?r.apply(this,arguments):"function"==typeof i?i.apply(this,arguments):n}},callCallbacks:function(e,t){t&&t.options.onceOnly&&1==t.firedElems.length&&(e=[e[0]]);for(var n,r=0;n=e[r];r++)n&&n.callback&&n.callback.call(n.elem,n.elem);t&&t.options.onceOnly&&1==t.firedElems.length&&t.me.unbindEventWithSelectorAndCallback.call(t.target,t.selector,t.callback)},checkChildNodesRecursively:function(e,t,n,r){for(var i,o=0;i=e[o];o++)n(i,t,r)&&r.push({callback:t.callback,elem:i}),i.childNodes.length>0&&l.checkChildNodesRecursively(i.childNodes,t,n,r)},mergeArrays:function(e,t){var n,r={};for(n in e)e.hasOwnProperty(n)&&(r[n]=e[n]);for(n in t)t.hasOwnProperty(n)&&(r[n]=t[n]);return r},toElementsArray:function(t){return n===t||"number"==typeof t.length&&t!==e||(t=[t]),t}}}(),c=function(){var e=function(){this._eventsBucket=[],this._beforeAdding=null,this._beforeRemoving=null};return e.prototype.addEvent=function(e,t,n,r){var i={target:e,selector:t,options:n,callback:r,firedElems:[]};return this._beforeAdding&&this._beforeAdding(i),this._eventsBucket.push(i),i},e.prototype.removeEvent=function(e){for(var t,n=this._eventsBucket.length-1;t=this._eventsBucket[n];n--)if(e(t)){this._beforeRemoving&&this._beforeRemoving(t);var r=this._eventsBucket.splice(n,1);r&&r.length&&(r[0].callback=null)}},e.prototype.beforeAdding=function(e){this._beforeAdding=e},e.prototype.beforeRemoving=function(e){this._beforeRemoving=e},e}(),a=function(t,r){var i=new c,o=this,a={fireOnAttributesModification:!1};return i.beforeAdding(function(n){var i,l=n.target;(l===e.document||l===e)&&(l=document.getElementsByTagName("html")[0]),i=new MutationObserver(function(e){r.call(this,e,n)});var c=t(n.options);i.observe(l,c),n.observer=i,n.me=o}),i.beforeRemoving(function(e){e.observer.disconnect()}),this.bindEvent=function(e,t,n){t=l.mergeArrays(a,t);for(var r=l.toElementsArray(this),o=0;o<r.length;o++)i.addEvent(r[o],e,t,n)},this.unbindEvent=function(){var e=l.toElementsArray(this);i.removeEvent(function(t){for(var r=0;r<e.length;r++)if(this===n||t.target===e[r])return!0;return!1})},this.unbindEventWithSelectorOrCallback=function(e){var t,r=l.toElementsArray(this),o=e;t="function"==typeof e?function(e){for(var t=0;t<r.length;t++)if((this===n||e.target===r[t])&&e.callback===o)return!0;return!1}:function(t){for(var i=0;i<r.length;i++)if((this===n||t.target===r[i])&&t.selector===e)return!0;return!1},i.removeEvent(t)},this.unbindEventWithSelectorAndCallback=function(e,t){var r=l.toElementsArray(this);i.removeEvent(function(i){for(var o=0;o<r.length;o++)if((this===n||i.target===r[o])&&i.selector===e&&i.callback===t)return!0;return!1})},this},s=function(){function e(e){var t={attributes:!1,childList:!0,subtree:!0};return e.fireOnAttributesModification&&(t.attributes=!0),t}function t(e,t){e.forEach(function(e){var n=e.addedNodes,i=e.target,o=[];null!==n&&n.length>0?l.checkChildNodesRecursively(n,t,r,o):"attributes"===e.type&&r(i,t,o)&&o.push({callback:t.callback,elem:i}),l.callCallbacks(o,t)})}function r(e,t){return l.matchesSelector(e,t.selector)&&(e._id===n&&(e._id=o++),-1==t.firedElems.indexOf(e._id))?(t.firedElems.push(e._id),!0):!1}var i={fireOnAttributesModification:!1,onceOnly:!1,existing:!1};f=new a(e,t);var c=f.bindEvent;return f.bindEvent=function(e,t,r){n===r?(r=t,t=i):t=l.mergeArrays(i,t);var o=l.toElementsArray(this);if(t.existing){for(var a=[],s=0;s<o.length;s++)for(var u=o[s].querySelectorAll(e),f=0;f<u.length;f++)a.push({callback:r,elem:u[f]});if(t.onceOnly&&a.length)return r.call(a[0].elem,a[0].elem);setTimeout(l.callCallbacks,1,a)}c.call(this,e,t,r)},f},u=function(){function e(){var e={childList:!0,subtree:!0};return e}function t(e,t){e.forEach(function(e){var n=e.removedNodes,i=[];null!==n&&n.length>0&&l.checkChildNodesRecursively(n,t,r,i),l.callCallbacks(i,t)})}function r(e,t){return l.matchesSelector(e,t.selector)}var i={};d=new a(e,t);var o=d.bindEvent;return d.bindEvent=function(e,t,r){n===r?(r=t,t=i):t=l.mergeArrays(i,t),o.call(this,e,t,r)},d},f=new s,d=new u;t&&i(t.fn),i(HTMLElement.prototype),i(NodeList.prototype),i(HTMLCollection.prototype),i(HTMLDocument.prototype),i(Window.prototype);var h={};return r(f,h,"unbindAllArrive"),r(d,h,"unbindAllLeave"),h}}(window,"undefined"==typeof jQuery?null:jQuery,void 0);
+
+function SoundProfiles() {
+  if (!Spicetify.LocalStorage || !Spicetify.Menu || !Spicetify.Player || !Spicetify.showNotification) {
+    setTimeout(SoundProfiles, 1000);
+    return;
+  }
+  var controls = document.getElementsByClassName('ExtraControls')[0];
+
+  var SoundProfiles = [];
+
+  for (var i = 3; i > 0; i--) {
+    var volume = Spicetify.LocalStorage.get("SoundProfile" + i);
+    if (isNaN(volume)) {
+      switch (i) {
+        case 1:
+          volume = "0.5";
+          break;
+        case 2:
+          volume = "0.3";
+          break;
+        case 3:
+          volume = "0.8";
+          break;
+      }
+      Spicetify.LocalStorage.set("SoundProfile" + i, volume);
+    }
+
+    var path;
+    switch (i) {
+      case 1:
+        path = "M10.04 5.984l.658-.77q.548.548.858 1.278.31.73.31 1.54 0 .54-.144 1.055-.143.516-.4.957-.259.44-.624.805l-.658-.77q.825-.865.825-2.047 0-1.183-.825-2.048zM0 11.032v-6h2.802l5.198-3v12l-5.198-3H0zm7 1.27v-8.54l-3.929 2.27H1v4h2.071L7 12.302z";
+        break;
+      case 2:
+        path = "M0 11.032v-6h2.802l5.198-3v12l-5.198-3H0zm7 1.27v-8.54l-3.929 2.27H1v4h2.071L7 12.302zm4.464-2.314q.401-.925.401-1.956 0-1.032-.4-1.957-.402-.924-1.124-1.623L11 3.69q.873.834 1.369 1.957.496 1.123.496 2.385 0 1.262-.496 2.385-.496 1.123-1.369 1.956l-.659-.762q.722-.698 1.123-1.623z";
+        break;
+      case 3:
+        path = "M12.945 1.379l-.652.763c1.577 1.462 2.57 3.544 2.57 5.858s-.994 4.396-2.57 5.858l.651.763a8.966 8.966 0 00.001-13.242zm-2.272 2.66l-.651.763a4.484 4.484 0 01-.001 6.397l.651.763c1.04-1 1.691-2.404 1.691-3.961s-.65-2.962-1.69-3.962zM0 5v6h2.804L8 14V2L2.804 5H0zm7-1.268v8.536L3.072 10H1V6h2.072L7 3.732z";
+        break;
+    }
+
+    var newDiv = document.createElement('div');
+    newDiv.id = "SoundProfile" + i;
+    newDiv.innerHTML = `
+        <button class="control-button" aria-label="Sound Profile ${i}" title="Sound Profile ${i}" style="top:2px;">
+          <svg role="img" height="16" width="16" viewBox="0 0 16 16" style="fill:currentcolor">
+            <path d="${path}">
+            </path>
+          </svg>
+        </button>`;
+    newDiv.addEventListener("click", function() {
+      volume = Spicetify.LocalStorage.get(this.id);
+      Spicetify.Player.setVolume(parseFloat(volume));
+    });
+
+    newDiv.addEventListener("contextmenu", function() {
+      if (confirm(`Save current volume to ${this.firstElementChild.title} ?`)) {
+        var sound = Spicetify.Player.getVolume();
+        Spicetify.LocalStorage.set(this.id, sound.toString());
+      }
+    })
+    controls.insertBefore(newDiv, controls.firstElementChild);
+  }
+  Arrive.unbindAllArrive();
+}
+
+//The arrive is necessary because it seems like spotify loads a first page first
+//but I need the second one
+document.arrive(".ReactModalPortal", SoundProfiles);
